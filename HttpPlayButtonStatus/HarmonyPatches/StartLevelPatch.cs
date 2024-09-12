9cbc7fd622b132bb59a27aec7ad661a8ab31b1c5
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-09-12 21:32:05 +0900

    Ver1.35.0対応

--- HttpPlayButtonStatus/HarmonyPatches/StartLevelPatch.cs
+++ HttpPlayButtonStatus/HarmonyPatches/StartLevelPatch.cs
@@ -1,11 +1,10 @@
 ﻿using HarmonyLib;
 using System;
-using System.Reflection;
 
 namespace HttpPlayButtonStatus.HarmonyPatches
 {
     [HarmonyPatch(typeof(SinglePlayerLevelSelectionFlowCoordinator))]
-    [HarmonyPatch("StartLevel", MethodType.Normal)]
+    [HarmonyPatch(nameof(SinglePlayerLevelSelectionFlowCoordinator.StartLevel), MethodType.Normal)]
     public class StartLevelPatch
     {
         public static event Action<bool> OnStartLevel;
@@ -13,9 +12,7 @@ namespace HttpPlayButtonStatus.HarmonyPatches
         public static string songName;
         static bool Prefix(bool practice, SinglePlayerLevelSelectionFlowCoordinator __instance)
         {
-            var type = __instance.GetType();
-            var property = type.GetProperty("selectedBeatmapLevel", BindingFlags.GetProperty | BindingFlags.NonPublic | BindingFlags.Instance);
-            var previewBeatmapLevel = (IPreviewBeatmapLevel)(property.GetValue(__instance));
+            var previewBeatmapLevel = __instance.selectedBeatmapLevel;
             Plugin.Log.Info($"StartLevel:{previewBeatmapLevel.levelID}");
             levelID = previewBeatmapLevel.levelID;
             songName = previewBeatmapLevel.songName;
